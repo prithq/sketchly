a499d570cf167058679dd1b99744efceb8a0896e
Prit Patel <projects.pritpatel@gmail.com>
2026-06-18 21:33:05 +0530

    line and circle tool

--- apps/web/components/Canvas.tsx
+++ apps/web/components/Canvas.tsx
@@ -2,8 +2,9 @@
 
 import { useEffect, useRef,useState } from "react";
 import { Shape } from "@repo/common/types";
-import Toolbar from "./Toolbar";
+import Toolbar from "@/components/Toolbar";
 import {drawShape} from "@repo/common/drawShape"
+
 
 
 export default function Canvas({ slug }: { slug: string }) {
@@ -27,6 +28,7 @@ export default function Canvas({ slug }: { slug: string }) {
     );
 
     const data = await res.json();
+    console.log(data)
 
     shapes.current = data.map(
       (shape: any) => shape.shapeData.shape
@@ -76,38 +78,16 @@ export default function Canvas({ slug }: { slug: string }) {
     ws.onmessage = (event) => {
       const message = JSON.parse(event.data);
 
-      if (message.type === "RECTANGLE") {
-        shapes.current.push(message.shape);
+     if(message.shape){
+  shapes.current.push(message.shape);
 
-        const canvas = canvasRef.current;
-        const ctx = canvas?.getContext("2d");
+  const canvas = canvasRef.current;
+  const ctx = canvas?.getContext("2d");
 
-        if (!canvas || !ctx) return;
+  if (!canvas || !ctx) return;
 
-        redraw(ctx, canvas);
-      }
-
-      if (message.type === "CIRCLE") {
-        shapes.current.push(message.shape);
-
-        const canvas = canvasRef.current;
-        const ctx = canvas?.getContext("2d");
-
-        if (!canvas || !ctx) return;
-
-        redraw(ctx, canvas);
-      }
-
-      if (message.type === "LINE") {
-        shapes.current.push(message.shape);
-
-        const canvas = canvasRef.current;
-        const ctx = canvas?.getContext("2d");
-
-        if (!canvas || !ctx) return;
-
-        redraw(ctx, canvas);
-      }
+  redraw(ctx, canvas);
+}
     };
 
     wsRef.current = ws;
@@ -118,88 +98,221 @@ export default function Canvas({ slug }: { slug: string }) {
   }, [slug]);
 
   // Canvas
-  useEffect(() => {
+
+  useEffect(()=>{
+
+    const canvas=canvasRef.current
+    if(!canvas)return
+    const ctx=canvas.getContext("2d")
+    if(!ctx)return
+
+  
+    canvas.width=window.innerWidth
+    canvas.height=window.innerHeight
+
+    ctx.strokeStyle="black"
 
 
-    if(tool==="CIRCLE"){
+    const handleMouseDown=(e:MouseEvent)=>{
 
-      const canvas=canvasRef.current
-      if(!canvas)return
+      
+  clicked.current = true;
+  startX.current = e.clientX;
+  startY.current = e.clientY;
 
-      const ctx=canvas?.getContext("2d")
+    }
 
-      if(!ctx)return
+    const handleMouseUp=(e:MouseEvent)=>{
 
-      canvas.width=window.innerWidth
-      canvas.height=window.innerHeight
+      if(tool==="RECTANGLE"){
 
-      ctx.strokeStyle="black"
+        if(!clicked.current)
+          return
 
-      const handleMouseDown=(e:MouseEvent)=>{
-
-        clicked.current=true
-        startX.current=e.clientX
-        startY.current=e.clientY
-
-
-        
-
-      }
-
-
-      const handleMouseUp=(e:MouseEvent)=>{
-
-        if(!clicked.current)return
         clicked.current=false
 
+        const width=e.clientX-startX.current
+        const height=e.clientY-startY.current
+
+        const shape: Shape = {
+          type: "RECTANGLE",
+          x: startX.current,
+          y: startY.current,
+          width,
+          height,
+        };
+
+        shapes.current.push(shape)
+
+        
+        wsRef.current?.send(
+          JSON.stringify({
+            type: "RECTANGLE",
+            shape,
+          })
+        );
         
         redraw(ctx,canvas)
 
+      }
+
+
+      if(tool==="CIRCLE"){
+
+        if(!clicked.current){
+
+          return
+        }
+        clicked.current=false
+
+        const width=e.clientX-startX.current
+        const height=e.clientY-startY.current
 
         
-        
-        
+        const shape:Shape={
+          type:"CIRCLE",
+          x:startX.current,
+          y:startY.current,
+          width,
+          height
+        }
+        shapes.current.push(shape)
+
+        wsRef.current?.send(JSON.stringify({
+          type:"CIRCLE",
+          shape
+        }))
+
+        redraw(ctx,canvas)
       }
-      
-      const handleMouseMove=(e:MouseEvent)=>{
-        
+
+
+      if(tool==="LINE"){
+
+        if(!clicked.current){
+          return
+        }
+
+        clicked.current=false
+
+        const endX=e.clientX
+        const endY=e.clientY
+
+        const shape:Shape={
+          type:"LINE",
+          startX:startX.current,
+          startY:startY.current,
+          endX,
+          endY
+        }
+
+        shapes.current.push(shape)
+
+        wsRef.current?.send(JSON.stringify({
+          type:"LINE",
+          shape
+        }))
+
+        redraw(ctx,canvas)
+
+      }
+
+
+
+
+
+
+    }
+
+    const handleMouseMove=(e:MouseEvent)=>{
+
+      if(tool==="RECTANGLE"){
+
         if(!clicked.current)return
 
-       
-
-
-        redraw(ctx,canvas)
-
+        const width=e.clientX-startX.current
+        const height=e.clientY-startY.current
         
 
-
-
-
+        const shape: Shape = {
+          type: "RECTANGLE",
+          x: startX.current,
+          y: startY.current,
+          width,
+          height,
+        };
+       redraw(ctx,canvas)
+       
+      ctx.strokeRect(shape.x,shape.y,shape.width,shape.height)
 
 
       }
 
-      canvas.addEventListener("mousedown",handleMouseDown)
-      canvas.addEventListener("mouseup",handleMouseUp)
-      canvas.addEventListener("mousemove",handleMouseMove)
-
       
 
-      return () => {
-      canvas.removeEventListener(
-        "mousedown",
-        handleMouseDown
-      );
-      canvas.removeEventListener(
-        "mouseup",
-        handleMouseUp
-      );
-      canvas.removeEventListener(
-        "mousemove",
-        handleMouseMove
-      );
-    };
 
+      if(tool==="CIRCLE"){
+
+          if(!clicked.current){
+
+          return
+        }
+        
+
+        const width=e.clientX-startX.current
+        const height=e.clientY-startY.current
+
+        
+        const shape:Shape={
+          type:"CIRCLE",
+          x:startX.current,
+          y:startY.current,
+          width,
+          height
+        }
+
+        redraw(ctx,canvas)
+        ctx.beginPath()
+        ctx.ellipse(
+          startX.current+width/2,
+          startY.current+height/2,
+          Math.abs(width/2),
+          Math.abs(height/2),0,0,Math.PI*2
+        )
+
+        ctx.stroke()
+
+
+
+        
+
+      }
+
+
+      if(tool==="LINE"){
+
+         if(!clicked.current){
+          return
+        }
+
+        clicked.current=true
+
+        const endX=e.clientX
+        const endY=e.clientY
+
+        const shape:Shape={
+          type:"LINE",
+          startX:startX.current,
+          startY:startY.current,
+          endX,
+          endY
+        }
+        redraw(ctx,canvas)
+        drawShape(ctx,shape)
+
+        
+
+      }
 
 
 
@@ -209,75 +322,10 @@ export default function Canvas({ slug }: { slug: string }) {
     }
 
 
+    canvas.addEventListener("mousedown",handleMouseDown)
+    canvas.addEventListener("mouseup",handleMouseUp)
+    canvas.addEventListener("mousemove",handleMouseMove)
 
-      if(tool==="RECTANGLE"){
-
-        const canvas = canvasRef.current;
-
-    if (!canvas) return;
-
-    const ctx = canvas.getContext("2d");
-
-    if (!ctx) return;
-
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-
-    ctx.strokeStyle = "black";
-
-    const handleMouseDown = (e: MouseEvent) => {
-      clicked.current = true;
-      startX.current = e.clientX;
-      startY.current = e.clientY;
-    };
-
-    const handleMouseUp = (e: MouseEvent) => {
-      if (!clicked.current) return;
-
-      clicked.current = false;
-
-      const width = e.clientX - startX.current;
-      const height = e.clientY - startY.current;
-
-      const shape = {
-        type: "RECTANGLE" as const,
-        x: startX.current,
-        y: startY.current,
-        width,
-        height,
-      };
-
-      shapes.current.push(shape);
-
-      wsRef.current?.send(
-        JSON.stringify({
-          type: "RECTANGLE",
-          shape,
-        })
-      );
-
-      redraw(ctx, canvas);
-    };
-
-    const handleMouseMove = (e: MouseEvent) => {
-      if (!clicked.current) return;
-
-      const width = e.clientX - startX.current;
-      const height = e.clientY - startY.current;
-
-      redraw(ctx, canvas);
-
-      ctx.strokeRect(
-        startX.current,
-        startY.current,
-        width,
-        height
-      );
-    };
-
-    canvas.addEventListener("mousedown", handleMouseDown);
-    canvas.addEventListener("mouseup", handleMouseUp);
-    canvas.addEventListener("mousemove", handleMouseMove);
 
     return () => {
       canvas.removeEventListener(
@@ -293,14 +341,11 @@ export default function Canvas({ slug }: { slug: string }) {
         handleMouseMove
       );
     };
+   
 
 
 
-
-      }
-
-    
-  }, []);
+  },[tool])
 
   return (
     <>
